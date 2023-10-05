a6693b700e7061314903f23b58723c397d2632f2
Alejandro <alejo@faztweb.com>
2023-10-05 14:23:45 -0500

    Piruja

--- App.java
+++ App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("¡HOLA MUNDO!");
+        System.out.println("Que tal piruja");
+        System.out.println("¿Como estas?");
     }
 }
